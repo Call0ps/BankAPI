aa76b6f2d8b777240a08cf207673593ef2722d5b
Carl Emil Lidbom <lidbom.calle@outlook.com>
2021-09-23 13:47:44 +0200

    Create useraccount

--- TestBankAPI/UnitTest1.cs
+++ TestBankAPI/UnitTest1.cs
@@ -6,6 +6,7 @@ namespace TestBankAPI
     public class Tests
     {
         private HelloWorldService service = new();
+        private UserAccountService uaService = new();
 
         [SetUp]
         public void Setup()
@@ -16,6 +17,42 @@ namespace TestBankAPI
         public void HelloWorldTest()
         {
             Assert.IsTrue(service.HelloWorld() == "Hello World");
+        }
+
+        [Test]
+        public void UserAccTest()
+        {
+            var user = new UserAccount(id: 1, email: "lidbom.calle@gmail.com");
+            var result = uaService.CreateAccount(id: 1, email: "lidbom.calle@gmail.com");
+            Assert.AreEqual(user, result);
+        }
+    }
+
+    internal class UserAccountService
+    {
+        internal UserAccount CreateAccount(int id, string email)
+        {
+            return new UserAccount(id, email);
+        }
+    }
+
+    internal class UserAccount
+    {
+        private int id;
+        private string email;
+
+        public UserAccount(int id, string email)
+        {
+            this.id = id;
+            this.email = email;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as UserAccount;
+            if (this.id == other.id && this.email == other.email)
+                return true;
+            return false;
         }
     }
 }
