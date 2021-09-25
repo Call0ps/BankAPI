904f76854046ca9c22b00817fa76e3050db27d95
Carl Emil Lidbom <lidbom.calle@outlook.com>
2021-09-25 02:02:30 +0200

    Code cleanup

--- BankAPI/Controllers/HelloWorldController.cs
+++ BankAPI/Controllers/HelloWorldController.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BankAPI.Services;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BankAPI.Services;
 
 namespace BankAPI.Controllers
 {
