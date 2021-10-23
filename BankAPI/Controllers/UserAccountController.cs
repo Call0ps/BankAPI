bfecc2c8470139345b9c4589413088f51f1d2859
Carl Emil Lidbom <lidbom.calle@outlook.com>
2021-10-23 02:00:11 +0200

    Create account endpoint

--- BankAPI/Controllers/UserAccountController.cs
+++ BankAPI/Controllers/UserAccountController.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+using BankAPI.Models;
 using BankAPI.Services;
 using BankAPI.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -8,10 +10,27 @@ namespace BankAPI.Controllers
     public class UserAccountController : ControllerBase
     {
         private readonly UserAccountService _userAccountService = new UserAccountService(new UserAccountRepository());
+
         [HttpGet]
         public IActionResult Get()
         {
             return Ok("Hello!");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewAccount newAccount)
+        {
+            var newU = _userAccountService.CreateAccount(newAccount.Id, newAccount.Email);
+            if (_userAccountService.Insert(newU))
+                return CreatedAtAction(nameof(Create), newU);
+            else
+                return BadRequest();
+        }
+
+        public class NewAccount
+        {
+            public int Id { get; set; }
+            public string Email { get; set; }
         }
     }
 }
