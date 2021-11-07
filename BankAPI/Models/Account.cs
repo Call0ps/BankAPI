b2cff317de482c57f6173ef8ca52487fcccd835b
Carl Emil Lidbom <lidbom.calle@outlook.com>
2021-11-08 00:11:00 +0100

    Worked on Account.cs

--- BankAPI/Models/Account.cs
+++ BankAPI/Models/Account.cs
@@ -1,12 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 namespace BankAPI.Models
 {
-    public record Account
+    public class Account
     {
         public Guid Guid { get; set; }
-        public double Balance {get; set; }
-        public double Interest { get; set; }
-    };
+        public double Balance {get; private set; }
+        public double Interest { get; private set; }
+        private List<double> _historicBalance = new List<double>();
+
+        public double AccruedInterest
+        {
+            get
+            {
+                double accruedInterest = 0;
+
+                foreach (var balance in _historicBalance)
+                {
+                    accruedInterest += balance * Interest;
+                }
+
+                return accruedInterest;
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+            _historicBalance.Add(Balance);
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+            _historicBalance.Add(Balance);
+        }
+
+        public void UpdateInterest(double percentage)
+        {
+            Interest = percentage;
+        }
+    }
 }
 
